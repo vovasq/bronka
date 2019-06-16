2c237872fb711f83d146f6b2ad691cbfd5b5e15c
vovasq <vova.balsutev@mail.ru>
2019-06-17 00:30:17 +0300

    spring config for business tests

--- server/src/main/kotlin/com/bronka/server/repository/UserRepositoryJpa.kt
+++ server/src/main/kotlin/com/bronka/server/repository/UserRepositoryJpa.kt
@@ -2,6 +2,8 @@ package com.bronka.server.repository
 
 import com.bronka.server.entity.UserAccount
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.NoRepositoryBean
+import org.springframework.stereotype.Repository
 
 interface UserRepositoryJpa : JpaRepository<UserAccount, Long> {
     fun findByName(name: String): List<UserAccount>
